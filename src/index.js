396750beaeb977dd552a7c003d7a3fbc3d0fd95e
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-07-18 13:04:51 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
-// import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// if (process.env.NODE_ENV === "production") disableReactDevTools();
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
